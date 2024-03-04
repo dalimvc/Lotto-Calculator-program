141fe9cd57a26c37196bbe8f1cfada39766eaac4
dalimvc <dalibor_mirkovic@live.com>
2024-03-04 11:38:33 +0100

    Add project files.

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Calculator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        string firstNumber = "0";
+        string secondNumber = "0";
+        int currentValue;
+        bool newNumber = true;
+        bool errorPresent = false;
+        bool divisinWithZero = false;
+        string buttonValueOperation;
+        string previousOperation;
+
+        //method to assign numbers to variables when user clicks on number buttons
+        //currentValue is current result; first and secons numbers are result of user input
+        //if a number is first imput it will be assihned to fisrtNumber, othervise it will be assigned to the second variable
+        private void AppendNumber(string number)
+        {
+            errorPresent = false;
+            divisinWithZero = false;
+            if (newNumber)
+            {
+                firstNumber = firstNumber + number;
+                try
+                {
+                    currentValue = int.Parse(firstNumber);
+                    ResultData.Text = currentValue.ToString();
+                }
+                catch
+                {
+                    ResultData.Text = "Number is too large.";
+                }
+            }
+            else
+            {
+                secondNumber = secondNumber + number;
+                try
+                {
+                    currentValue = int.Parse(secondNumber);
+                    ResultData.Text = currentValue.ToString();
+                }
+                catch
+                {
+                    ResultData.Text = "Number is too large.";
+                }
+            }
+        }
+
+        //based on chosen operation different operation will be performed
+        //checked is used to check if number is too large and it would return an error message in that case
+        //current value will be assigned the value of result after aritmetic operation is performed
+        public void CalculateResult()
+        {
+            errorPresent = false;
+            divisinWithZero = false;
+            if (!string.IsNullOrEmpty(previousOperation) && secondNumber != "0")
+            {
+                if (previousOperation == "+")
+                {
+                    try
+                    {
+                        checked
+                        {
+                            currentValue = int.Parse(firstNumber) + int.Parse(secondNumber);
+                        }
+                    }
+                    catch (OverflowException)
+                    {
+                        errorPresent = true;
+                        newNumber = true;
+                        firstNumber = "";
+                        secondNumber = "";
+                        currentValue = 0;
+                    }
+                }
+                else if (previousOperation == "-")
+                {
+                    try
+                    {
+                        checked
+                        {
+                            currentValue = int.Parse(firstNumber) - int.Parse(secondNumber);
+                        }
+                    }
+                    catch (OverflowException)
+                    {
+                        errorPresent = true;
+                        newNumber = true;
+                        firstNumber = "";
+                        secondNumber = "";
+                        currentValue = 0;
+                    }
+                    catch (FormatException)
+                    {
+                        currentValue = 0;
+                        newNumber = true;
+                        firstNumber = "0";
+                        secondNumber = "0";
+                        currentValue = 0;
+                    }
+                }
+                else if (previousOperation == "*")
+                {
+                    try
+                    {
+                        checked
+                        {
+                            currentValue = int.Parse(firstNumber) * int.Parse(secondNumber);
+                        }
+                    }
+                    catch (OverflowException)
+                    {
+                        errorPresent = true;
+                        newNumber = true;
+                        firstNumber = "";
+                        secondNumber = "";
+                        currentValue = 0;
+                    }
+                    catch (FormatException)
+                    {
+                        currentValue = 0;
+                        newNumber = true;
+                        firstNumber = "0";
+                        secondNumber = "0";
+                        currentValue = 0;
+                    }
+                }
+                else if (previousOperation == "/" && secondNumber != "0" && secondNumber != "00")
+                {
+                    Debug.WriteLine(secondNumber);
+                    currentValue = int.Parse(firstNumber) / int.Parse(secondNumber);
+                }
+                firstNumber = currentValue.ToString();
+                secondNumber = "";
+            }
+            else if (previousOperation == "/" && secondNumber == "0")
+            {
+                divisinWithZero = true;
+            }
+        }
+
+        //method, on click registers users input- numbers
+        //it calls AppendNumber method with button value
+        private void ButtonNumber(object sender, RoutedEventArgs e)
+        {
+            var buttonValue = (Button)sender;
+            AppendNumber(buttonValue.Content.ToString());
+        }
+
+        //here buuton on click operation is detected
+        //based on that value different aritmetic operation will be performed
+        //error messages are handled here
+        private void ButtonOperation(object sender, RoutedEventArgs e)
+        {
+            newNumber = false;
+            var buttonValue = (Button)sender;
+            buttonValueOperation = buttonValue.Content.ToString();
+            CalculateResult();
+            previousOperation = buttonValueOperation;
+            if (divisinWithZero)
+            {
+                ResultData.Text = "Error: Division by zero";
+                newNumber = true;
+                firstNumber = "";
+                secondNumber = "";
+                currentValue = 0;
+                previousOperation = null;
+            }
+            else if (errorPresent)
+            {
+                ResultData.Text = "Number is too large.";
+            }
+            else
+            {
+                ResultData.Text = currentValue.ToString();
+            }
+        }
+
+        //it works in a similar way as previuos method
+        //when user clicks on "=" an operation will be performed by calling CalculateResult() method
+        //error massages are handled here
+        private void ButtonEquals(object sender, RoutedEventArgs e)
+        {
+            CalculateResult();
+            if (divisinWithZero)
+            {
+                ResultData.Text = "Error: Division by zero";
+                newNumber = true;
+                firstNumber = "";
+                secondNumber = "";
+                currentValue = 0;
+                previousOperation = null;
+            }
+            else if (errorPresent)
+            {
+                ResultData.Text = "Number is too large.";
+            }
+            else
+            {
+                ResultData.Text = firstNumber.ToString();
+            }
+            previousOperation = null;
+        }
+
+        //all data is reset by calling this method
+        private void ButtonC(object sender, RoutedEventArgs e)
+        {
+            newNumber = true;
+            firstNumber = "0";
+            secondNumber = "0";
+            currentValue = 0;
+            ResultData.Text = currentValue.ToString();
+            previousOperation = null;
+        }
+    }
+}
---
+++ Lottoprogram/MainPage.xaml.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Lottoprogram
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        string errorMessage;
+        int rounds;
+        int fives = 0;
+        int sixes = 0;
+        int sevens = 0;
+        bool checkInput;
+        Random randomNumber = new Random();
+        List<int> enteredLottoNumbers = new List<int>();
+        List<List<int>> generatedLottoSeries = new List<List<int>>();
+
+        //Method gets values from users' input and tries to parse it as an int and if successfull adds those values into a list
+        //otherwise error message if uppdated
+        //checkInput will be used to update result, when the first time user has entered correct input but some of the following times input is not correct
+        //between different round list is erased and the same goes for the error message
+        public void GetNumbers()
+        {
+            errorMessage = string.Empty;
+            enteredLottoNumbers.Clear();
+            try
+            {
+                int.TryParse(number1.Text, out int value1);
+                int.TryParse(number2.Text, out int value2);
+                int.TryParse(number3.Text, out int value3);
+                int.TryParse(number4.Text, out int value4);
+                int.TryParse(number5.Text, out int value5);
+                int.TryParse(number6.Text, out int value6);
+                int.TryParse(number7.Text, out int value7);
+                enteredLottoNumbers.Add(value1);
+                enteredLottoNumbers.Add(value2);
+                enteredLottoNumbers.Add(value3);
+                enteredLottoNumbers.Add(value4);
+                enteredLottoNumbers.Add(value5);
+                enteredLottoNumbers.Add(value6);
+                enteredLottoNumbers.Add(value7);
+            }
+            catch
+            {
+                checkInput = false;
+                errorMessage = "Alla fält är obligatoriska och du kan endast ange nummer mellan 1-35!";
+                ErrorMessageTextBlock.Text = errorMessage;
+            }
+        }
+
+        //works the same way as the previous method, only this time, the method tries to parse number of rounds
+        public void GetNrOfRounds()
+        {
+            try
+            {
+                int.TryParse(nrDraws.Text, out rounds);
+            }
+            catch
+            {
+                checkInput = false;
+                errorMessage = "Antal dragningar måste vara ett positivt nummer!";
+                ErrorMessageTextBlock.Text = errorMessage;
+            }
+        }
+
+        //even if parsing was successful, input might be wrong; 2 or more numbers can be the same or they can be 0 or less or bigger than 35
+        //number of round can not be 0 or less than 0
+        //method checks input and if it is not correct appropriate error message will be shown
+        public void CheckNumberValidity()
+        {
+            for (int i = 0; i < enteredLottoNumbers.Count - 1; i++)
+            {
+                if (enteredLottoNumbers[i] < 1 || enteredLottoNumbers[i] > 35)
+                {
+                    checkInput = false;
+                    errorMessage = "Numren måste vara mellan 1 och 35! Felaktig inmatning.";
+                }
+                else if (enteredLottoNumbers[i] == enteredLottoNumbers[i + 1])
+                {
+                    checkInput = false;
+                    errorMessage = "Du kan inte ange samma nummer mer än en gång! Felaktig inmatning.";
+                }
+                else if (rounds <= 0)
+                {
+                    checkInput = false;
+                    errorMessage = "Fältet är obligatoriskt och det måste vara ett positivt nummer! Felaktig inmatning.";
+                }
+            }
+            ErrorMessageTextBlock.Text = errorMessage;
+        }
+
+        //method that generates one lotto series with 7 numbers between 1 and 35
+        //method returns an list
+        public List<int> generateLottoNumbers()
+        {
+            List<int> generatedLottoNumbers = new List<int>();
+            generatedLottoNumbers.Add(randomNumber.Next(1, 35));
+            int counter = 0;
+            while (counter < 6)
+            {
+                int lottoNumber = randomNumber.Next(1, 35);
+                if (!generatedLottoNumbers.Contains(lottoNumber))
+                {
+                    generatedLottoNumbers.Add(lottoNumber);
+                    counter++;
+                }
+            }
+            return generatedLottoNumbers;
+        }
+
+        //method that generates n-amount of lotto series based on users input "antal dragningar"
+        //method returns list of list type int
+        //between 2 rounds list is cleared
+        public List<List<int>> generateLottoSeriers()
+        {
+            generatedLottoSeries.Clear();
+            int counter = 0;
+            while (rounds > counter)
+            {
+                generatedLottoSeries.Add(generateLottoNumbers());
+                counter++;
+            }
+            return generatedLottoSeries;
+        }
+
+        //counting number if matches between users input and computr's generated lotto series
+        //it loops through outer list and than throught each inner list that contains 1 lotto series
+        //method checks in each inner list how many mathes there are with user's input
+        //than number of mathes will uppdate
+        //first checks 7s, than 6s and at the end 5s in order to awoid 7s being counted in 5s and 6s
+        public int CountOccurrences()
+        {
+            sevens = 0;
+            sixes = 0;
+            fives = 0;
+            int counterMatches = 0;
+            foreach (List<int> lottoCombination in generatedLottoSeries)
+            {
+                counterMatches = 0;
+                foreach (int number in enteredLottoNumbers)
+                {
+                    if (lottoCombination.Contains(number))
+                    {
+                        counterMatches++;
+                    }
+                    if (counterMatches == 7)
+                    {
+                        sevens++;
+                    }
+                    else if (counterMatches == 6)
+                    {
+                        sixes++;
+                    }
+                    else if (counterMatches == 5)
+                    {
+                        fives++;
+                    }
+                }
+            }
+            return counterMatches;
+        }
+
+        //based on result from a method CountOccurrences(), text that shows matches updates
+        //if...else statement is for the case user enters correct input than wrong input
+        //when it's wrong input results will be 0
+        public void updateTextresult()
+        {
+            if (checkInput == true)
+            {
+                fiveMatches.Text = fives.ToString();
+                sixMatches.Text = sixes.ToString();
+                SevenMatches.Text = sevens.ToString();
+            }
+            else
+            {
+                fiveMatches.Text = "0";
+                sixMatches.Text = "0";
+                SevenMatches.Text = "0";
+            }
+
+        }
+
+        //"main method" that runs all previously declared methods
+        //checkInput (user has entered correct input) is initialy true but gets changes in methods that check users' input
+        //if input is false methods where conputer generates lotto series won't be running
+        private void RunLotto(object sender, RoutedEventArgs e)
+        {
+            checkInput = true;
+            updateTextresult();
+            GetNumbers();
+            GetNrOfRounds();
+            CheckNumberValidity();
+            if (checkInput == true)
+            {
+                generateLottoSeriers();
+                CountOccurrences();
+            }
+            updateTextresult();
+        }
+    }
+}
